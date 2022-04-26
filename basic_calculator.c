bc9706c7e70d39cd3100dd4a1fcc7635d4ebfb81
Suyog <edusuyog@gmail.com>
2022-04-26 15:12:11 +0545

    First commit

---
+++ basic_calculator.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    double num1;
+    double num2,num3;
+    printf("enter first number:");
+    scanf("%lf",&num1);
+    printf("enter second number:");
+    scanf("%lf",&num2);
+    num3=num1+num2;
+    printf("the sum of two number is %f",num3);
+
+}
